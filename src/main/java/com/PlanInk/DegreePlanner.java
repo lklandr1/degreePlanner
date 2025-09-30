b2de128ef8126bb58ceeb1bb0a8dd27ad897b6f0
Braden <braden.q.dougherty@gmail.com>
2025-09-30 12:33:01 -0500

    Set up maven, and spring boot using thymeleaf

---
+++ src/main/java/com/PlanInk/DegreePlanner.java
@@ -0,0 +1,11 @@
+package com.PlanInk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegreePlanner {
+    public static void main(String[] args) {
+        SpringApplication.run(DegreePlanner.class, args);
+    }
+}
